095941d09ce09b934d06e08872140f8c3c6e37e8
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2013-09-05 16:12:46 -0700

    Configuration clean-up

--- manifests/site.pp
+++ manifests/site.pp
@@ -79,7 +79,7 @@ node default {
     ]:
   }
 
-  file { "${boxen::config::srcdir}/my-boxen":
+  file { "${boxen::config::srcdir}/personal/my-boxen":
     ensure => link,
     target => $boxen::config::repodir
   }
