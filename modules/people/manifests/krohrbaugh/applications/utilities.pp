8ee65f2a0775e2b6885156129753fc64d1e1a5d2
Kevin Rohrbaugh <kevin@rhrbgh.com>
2013-09-08 21:08:04 -0700

    Even moar apps

--- modules/people/manifests/krohrbaugh/applications/utilities.pp
+++ modules/people/manifests/krohrbaugh/applications/utilities.pp
@@ -1,6 +1,8 @@
 class people::krohrbaugh::applications::utilities {
   include alfred
   include appcleaner
+  include istatmenus4
+  include notational_velocity::nvalt
   include onepassword
   include spectacle
 }
