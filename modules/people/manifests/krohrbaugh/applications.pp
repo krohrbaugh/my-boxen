ded2d72d495066bbe1f4c59e9d4cd79a865ec268
Kevin Rohrbaugh <kevin@rhrbgh.com>
2013-09-08 20:45:53 -0700

    Add development tools & IDEs

--- modules/people/manifests/krohrbaugh/applications.pp
+++ modules/people/manifests/krohrbaugh/applications.pp
@@ -1,5 +1,6 @@
 class people::krohrbaugh::applications {
   include people::krohrbaugh::applications::browsers
+  include people::krohrbaugh::applications::development
   include people::krohrbaugh::applications::editors
   include people::krohrbaugh::applications::entertainment
   include people::krohrbaugh::applications::communication
