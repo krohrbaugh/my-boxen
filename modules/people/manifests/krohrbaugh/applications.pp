a7fe3ef195205f73b820c8da1871809a76b19796
Kevin Rohrbaugh <kevin@rhrbgh.com>
2013-09-08 19:40:29 -0700

    Add moar apps

--- modules/people/manifests/krohrbaugh/applications.pp
+++ modules/people/manifests/krohrbaugh/applications.pp
@@ -1,5 +1,7 @@
 class people::krohrbaugh::applications {
-  include people::krohrbaugh::applications::terminals
   include people::krohrbaugh::applications::editors
+  include people::krohrbaugh::applications::entertainment
+  include people::krohrbaugh::applications::communication
+  include people::krohrbaugh::applications::terminals
   include people::krohrbaugh::applications::utilities
 }
