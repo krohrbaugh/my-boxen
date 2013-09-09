04d57b1e8e784552167dbcbc5bee3cabfb0dc83b
Kevin Rohrbaugh <kevin@rhrbgh.com>
2013-09-08 20:23:31 -0700

    Add browsers

--- modules/people/manifests/krohrbaugh/applications.pp
+++ modules/people/manifests/krohrbaugh/applications.pp
@@ -1,4 +1,5 @@
 class people::krohrbaugh::applications {
+  include people::krohrbaugh::applications::browsers
   include people::krohrbaugh::applications::editors
   include people::krohrbaugh::applications::entertainment
   include people::krohrbaugh::applications::communication
