945aea2baaf3f3ae8a4f17fffa5e0ce39038f431
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2014-08-06 14:14:32 -0700

    Upgrade all the things

--- modules/people/manifests/krohrbaugh/config/keyboard.pp
+++ modules/people/manifests/krohrbaugh/config/keyboard.pp
@@ -1,11 +1,11 @@
 class people::krohrbaugh::config::keyboard {
   require people::krohrbaugh::config
 
-  require keyremap4macbook
-  require keyremap4macbook::login_item
-  require keyremap4macbook::config
+  require karabiner
+  require karabiner::login_item
+  require karabiner::config
 
-  keyremap4macbook::private_xml { "private.xml":
+  karabiner::private_xml { "private.xml":
     source  => "${people::krohrbaugh::config::dotfiles_dir}/osx/key_remap.xml",
     require => Repository[$people::krohrbaugh::config::dotfiles_dir],
   }
@@ -17,14 +17,14 @@ class people::krohrbaugh::config::keyboard {
   private_mapping { "brightness_controls": }
 
   private_mapping { "power_sleep_button":
-    ensure => disabled,  
+    ensure => disabled,
   }
 
   private_mapping { "mission_control_dashboard":
     ensure => disabled,
   }
 
-  # The unless capability here should probably be submitted to 
+  # The unless capability here should probably be submitted to
   # boxen/puppet-keyremap4macbook to prevent the action from being executed on
   # every Boxen run.
   define private_mapping ($setting = $title, $ensure = "enabled") {
@@ -32,24 +32,24 @@ class people::krohrbaugh::config::keyboard {
 
     if $ensure == "enabled" {
       $action     = "enable"
-      $unless_cmd = "${keyremap4macbook::config::cli} changed | grep ${identifier}=1"
+      $unless_cmd = "${karabiner::config::cli} changed | grep ${identifier}=1"
       $onlyif_cmd = undef
     }
     else {
       $action     = "disable"
       $unless_cmd = undef
-      $onlyif_cmd = "${keyremap4macbook::config::cli} changed | grep ${identifier}=1"
+      $onlyif_cmd = "${karabiner::config::cli} changed | grep ${identifier}=1"
     }
 
-    $cmd = "${keyremap4macbook::config::cli} ${action} ${identifier}"
+    $cmd = "${karabiner::config::cli} ${action} ${identifier}"
 
     exec { "people::krohrbaugh::config::keyboard::${action}_${identifier}":
       command   => $cmd,
       unless    => $unless_cmd,
       onlyif    => $onlyif_cmd,
       require   => [
-        Package["KeyRemap4MacBook_${keyremap4macbook::config::version}"],
-        KeyRemap4MacBook::Private_xml["private.xml"],
+        Package["Karabiner_${karabiner::config::version}"],
+        Karabiner::Private_xml["private.xml"],
       ],
     }
   }
