4e973b3d89f23c8c507135130b22cc7bde2563d1
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2014-10-29 15:40:15 -0700

    Install latest Ruby (2.1.4)

--- manifests/site.pp
+++ manifests/site.pp
@@ -67,9 +67,7 @@ node default {
   include nodejs::v0_10
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.1.2': }
-  ruby::version { '2.1.3': }
+  ruby::version { '2.1.4': }
 
   # Install Bundler for all Ruby versions
   ruby_gem { 'bundler for all rubies':
