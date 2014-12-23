259cbf571bae70045b6e3ac071e94330ad1c0297
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2014-12-23 15:03:24 -0800

    Upgrade Sublime Text module

--- modules/people/manifests/krohrbaugh/applications/editors.pp
+++ modules/people/manifests/krohrbaugh/applications/editors.pp
@@ -3,20 +3,19 @@ class people::krohrbaugh::applications::editors {
 
   include textmate::textmate2::beta
 
-  include sublime_text_2
-  include sublime_text_2::config
-
-  sublime_text_2::package { "Package Control":
+  include sublime_text::v2
+  sublime_text::v2::package { 'Package Control':
     source => "wbond/sublime_package_control",
   }
 
-  # Install Sublime Text 2 settings
-  file { "${sublime_text_2::config::packagedir}/User":
+  # Install Sublime Text 2 settings (from dotfiles)
+  require sublime_text::v2::config
+  file { "${sublime_text::v2::config::packagedir}/User":
     ensure => link,
     target => "${people::krohrbaugh::config::dotfiles_dir}/sublime2/User",
-    require  => [
+    require => [
       Repository[$people::krohrbaugh::config::dotfiles_dir],
-      Class["sublime_text_2::config"],
+      Class["sublime_text::v2::config"],
     ],
   }
 }
