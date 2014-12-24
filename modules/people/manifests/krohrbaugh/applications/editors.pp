e7ade6138078497be34acccb2fd1340f25400894
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2014-12-24 12:31:39 -0800

    Add Sublime Text 3

--- modules/people/manifests/krohrbaugh/applications/editors.pp
+++ modules/people/manifests/krohrbaugh/applications/editors.pp
@@ -3,6 +3,22 @@ class people::krohrbaugh::applications::editors {
 
   include textmate::textmate2::beta
 
+  include sublime_text
+  sublime_text::package { 'Theme - Cobalt2':
+    source => "wesbos/cobalt2"
+  }
+
+  # Install Sublime Text 3 settings (from dotfiles)
+  require sublime_text::config
+  file { "${sublime_text::config::packagedir}/User":
+    ensure => link,
+    target => "${people::krohrbaugh::config::dotfiles_dir}/sublime3/User",
+    require => [
+      Repository[$people::krohrbaugh::config::dotfiles_dir],
+      Class["sublime_text::config"],
+    ],
+  }
+
   include sublime_text::v2
   sublime_text::v2::package { 'Package Control':
     source => "wbond/sublime_package_control",
