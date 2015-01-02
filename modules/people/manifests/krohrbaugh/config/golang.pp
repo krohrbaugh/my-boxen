aea6c1948f8726e2c650f3b65f6466325831e3ff
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2015-01-02 00:01:55 -0800

    Use separate directory for 3rd-party go libs

--- modules/people/manifests/krohrbaugh/config/golang.pp
+++ modules/people/manifests/krohrbaugh/config/golang.pp
@@ -1,8 +1,6 @@
 class people::krohrbaugh::config::golang {
+  # GOPATH setup
   $go_dir = "${boxen::config::srcdir}/go"
-  $go_bin = "${go_dir}/bin"
-  $go_pkg = "${go_dir}/pkg"
-  $go_src = "${go_dir}/src"
 
   file { $go_dir:
     owner   => $::boxen_user,
@@ -10,6 +8,45 @@ class people::krohrbaugh::config::golang {
     ensure  => directory,
     recurse => true,
   }
+
+  # 3rd party root
+  $go_3rdparty = "${go_dir}/3rdparty"
+  $go_3rdparty_bin = "${go_3rdparty}/bin"
+  $go_3rdparty_pkg = "${go_3rdparty}/pkg"
+  $go_3rdparty_src = "${go_3rdparty}/src"
+
+  file { $go_3rdparty:
+    owner   => $::boxen_user,
+    group   => staff,
+    ensure  => directory,
+    recurse => true,
+  }
+
+  file { $go_3rdparty_bin:
+    owner   => $::boxen_user,
+    group   => staff,
+    ensure  => directory,
+    recurse => true,
+  }
+
+  file { $go_3rdparty_pkg:
+    owner   => $::boxen_user,
+    group   => staff,
+    ensure  => directory,
+    recurse => true,
+  }
+
+  file { $go_3rdparty_src:
+    owner   => $::boxen_user,
+    group   => staff,
+    ensure  => directory,
+    recurse => true,
+  }
+
+  # Go root
+  $go_bin = "${go_dir}/bin"
+  $go_pkg = "${go_dir}/pkg"
+  $go_src = "${go_dir}/src"
 
   file { $go_bin:
     owner   => $::boxen_user,
